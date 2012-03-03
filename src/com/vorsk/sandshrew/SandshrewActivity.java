9e173a51300a5092639551620f7884cb910cf85c
Ian Foster <mrlanrat@gmail.com>
2012-03-02 19:00:16 -0800

    init

---
+++ src/com/vorsk/sandshrew/SandshrewActivity.java
@@ -0,0 +1,13 @@
+package com.vorsk.sandshrew;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SandshrewActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

a7c82e86e203c841b0242cd083f6ed701b171825
Lester Villaspir <lester.villaspir@gmail.com>
2012-03-03 05:04:51 -0800

    Checked to see if string is valid in Parser

--- src/com/vorsk/sandshrew/Parser.java
+++ src/com/vorsk/sandshrew/Parser.java
@@ -8,16 +8,23 @@ public class Parser {
 	/**
 	 * Returns the birthday by parsing a given string. The input string must be from an ID.
 	 * @param str String to be parsed
-	 * @return Birthday represented as the string "YYYYMMDD"
+	 * @return Birthday represented as the string "YYYYMMDD" or null if not a valid string
 	 */
 	public static String getBirthday(String str){
 		StringBuilder parse = new StringBuilder();
 		String subStr;
 		
-		subStr = str.substring(str.indexOf("="));
+		if (str.indexOf('=') == -1) return null;
+		
+		subStr = str.substring(str.indexOf('='));
+		if (subStr.length() != 14) return null;
 		parse.append(subStr.substring(5, 9));
 		parse.append(subStr.substring(3, 5));
 		parse.append(subStr.substring(11, 13));
+		
+		for (int i = 0; i < parse.length(); i++)
+			if ( !Character.isDigit(parse.charAt(i)))
+				return null;
 		
 		return parse.toString();
 		

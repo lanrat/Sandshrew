a278f94b07f41f0d391d97182938b52699daf56c
Anthony Mao <whs.fbla.amao@gmail.com>
2012-03-02 21:58:19 -0800

    Added a setBirthday method in AgeChecker

--- src/com/vorsk/sandshrew/AgeChecker.java
+++ src/com/vorsk/sandshrew/AgeChecker.java
@@ -38,6 +38,33 @@ public class AgeChecker
 		}
 	}
 	
+	//change the birthdays so we dont have to make a new class each time...
+	public void setBirthday(String birthdays)
+	{
+		year = Integer.parseInt(birthdays.substring(0,3));
+		legalYear = year + 21;
+		
+		//checks if the first value of month is 0
+		if(birthdays.valueOf(4).equals(0))
+		{
+			month = Integer.parseInt(birthdays.substring(5,5));
+		}
+		else
+		{
+			month = Integer.parseInt(birthdays.substring(4,5));
+		}
+		
+		//checks if the first value of day is 0
+		if(birthdays.valueOf(6).equals(0))
+		{
+			day = Integer.parseInt(birthdays.substring(7,7));
+		}
+		else
+		{
+			day = Integer.parseInt(birthdays.substring(6,7));
+		}
+	}
+	
 	//private method to setLegalAge, if the default 21 is not desired
 	private void setLegalAge(int i)
 	{

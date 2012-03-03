f58189268a8a16e1fc9c3378363e061ee36deb40
Anthony Mao <whs.fbla.amao@gmail.com>
2012-03-02 20:04:45 -0800

    adding age checker

---
+++ src/com/vorsk/sandshrew/AgeChecker.java
@@ -0,0 +1,28 @@
+package com.vorsk.sandshrew;
+//class to do age checking
+public class AgeChecker
+{
+	private int month;
+	private int day;
+	private int year;
+	private int legalYear;
+	private int legalAge = 21; 
+	public AgeChecker(int number)
+	{
+		
+	}
+	//helper method to calculate legal age
+	private void setLegalAge(int i)
+	{
+		legalAge = i;
+	}
+	public void calculateLegalAge(int i)
+	{
+		
+	}
+	
+	public boolean isLegal()
+	{
+		return false;
+	}
+}
